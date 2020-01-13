0793d1ac032ea9c582285661683f8d994e131530
Stephen Callum <stephen.callum@hotmail.co.uk>
2020-01-13 23:06:56 +0000

    Relocated Folder

---
+++ Assets/ChangeColour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+    Renderer rend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        
+    }
+
+    IEnumerator ColorChange()
+    {
+        rend.material.SetColor("_Color", UnityEngine.Random.ColorHSV());
+        yield return new WaitForSeconds(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ColorChange());
+    }
+}
